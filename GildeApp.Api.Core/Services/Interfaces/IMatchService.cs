9d934f652f65d8cde2b8bd2577d86243b5a4ea2b
KilianDegryse <kilian.degryse@student.howest.be>
2026-06-22 19:17:08 +0200

    Setup apicore services structure

---
+++ GildeApp.Api.Core/Services/Interfaces/IMatchService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildeApp.Api.Core.Services.Interfaces
+{
+    public interface IMatchService
+    {
+    }
+}
---
+++ GildeApp.Api.Core/Services/WeaponService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GildeApp.Api.Core.Services.Interfaces;
+
+namespace GildeApp.Api.Core.Services
+{
+    public class WeaponService : IWeaponService
+    {
+    }
+}
