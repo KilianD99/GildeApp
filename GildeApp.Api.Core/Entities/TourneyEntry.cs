85eca2228af25371faa0d7e6d9c783fc0746c927
KilianDegryse <kilian.degryse@student.howest.be>
2026-09-13 14:07:07 +0200

    Setup tourneyEntry

---
+++ GildeApp.Api.Core/Entities/TourneyEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildeApp.Api.Core.Entities
+{
+    public class TourneyEntry : BaseEntity
+    {
+        public Guid TourneyId { get; set; }
+        public Tourney Tourney { get; set; } = null;
+
+        public Guid PlayerId { get; set; }
+        public Player Player { get; set; } = null;
+
+        public int Position { get; set; }
+
+        public ICollection<Match> MatchesAsFirst { get; set; } = new List<Match>();
+        public ICollection<Match> MatchesAsSecond { get; set; } = new List<Match>();
+    }   
+}
